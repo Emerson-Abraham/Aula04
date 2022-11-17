578ec861836dcd047aa33ac7ffdd06fc2a0fd636
Emerson <prof.emerson.ti@gmail.com>
2022-11-16 21:35:46 -0300

    Modificacoes apos hard reset

--- src/NavegandoCommits.java
+++ src/NavegandoCommits.java
@@ -4,7 +4,6 @@ public class NavegandoCommits {
 	public static void main(String[] args) {
 		/*Cadê o conteúdo que estava aqui?*/
 		System.out.println("Nossa branch agora termina aqui");
-		
+		System.out.println("Tudo alterado com sucesso !!!");
 	}
-
 }
